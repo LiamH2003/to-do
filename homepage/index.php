5c128a43979489b5ecf9bf7c254f96ce789b8eb4
Liam Herman <liam.herman@hotmail.be>
2024-08-11 14:37:38 +0200

    feat - homepage: account and search

--- homepage/index.php
+++ homepage/index.php
@@ -5,16 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To Do</title>
     <link rel="stylesheet" href="../styles/style.css">
-    <link rel="stylesheet" href="../styles/homestyle.css">
+    <link rel="stylesheet" href="../styles/homestyle3.css">
     <link rel="icon" href="../images/to-do_icon.png" type="image/icon type">
     <script type="text/javascript" src="../script/homescript.js"></script>
 </head>
 <body>
     <div class="bodyDiv">
         <div class="mainDiv">
-            <div class="headerDiv"></div>
+            <div class="leftNavDiv">
+                <a class="innerLeftDiv" href="../start/index.php">
+                    <div class="iconDiv">
+                        <img src="../images/profile.png" width="70" height="70" alt="">
+                    </div>
+                    <div class="profileText">
+                        <h4>Example</h4>
+                        <span>Example@hotmail.be</span>
+                    </div>
+                    <div class="logoutDiv">
+                        <img src="../images/logout.png" width="40" height="40" alt="">
+                    </div>
+                </a>
+                <div class="searchbarDiv">
+                    <form action="#" method="get" class="searchForm">
+                        <input type="text" class="searchInput" placeholder="Search...">
+                    </form>
+                </div>
+            </div>
             <div class="contentDiv"></div>
-            <div class="footerDiv"></div>
+            <div class="rightNavDiv"></div>
         </div>
     </div>
 </body>
